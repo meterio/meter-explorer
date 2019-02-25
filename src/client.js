877684bed98b7bce388b0ac7ef978ba653dc2fbe
simonzg <zhanghan.simon@gmail.com>
2019-02-25 10:58:52 -0800

    changed the server address

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import { meterify } from "meterify";
 const Web3 = require("web3");
 
-const web3 = meterify(new Web3(), "http://192.168.1.193:8669");
+const web3 = meterify(new Web3(), "http://n01.uubright.com:8669");
 
 const getBlockLatest = () => {
   return web3.eth.getBlock("latest");
