0c0f61a8370f5212d4e0b61f6b7e23e40f8cdcb7
simonzg <zhanghan.simon@gmail.com>
2019-03-29 10:46:08 -0700

    rename token label

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -5,13 +5,13 @@
       <b-row>
         <b-col cols="2" class="field-name">
           <div>ID</div>
-          <div>Governance Token</div>
-          <div>Meter Token</div>
+          <div>Governance Balance</div>
+          <div>Meter Balance</div>
         </b-col>
         <b-col cols="10" class="field-value">
-          <div>{{$route.params.addr}}</div>
-          <div class="mtrg-amount">{{mtrgBalance | amount}}</div>
-          <div class="mtr-amount">{{mtrBalance | amount}}</div>
+          <div>{{ $route.params.addr }}</div>
+          <div class="mtrg-amount">{{ mtrgBalance | amount }}</div>
+          <div class="mtr-amount">{{ mtrBalance | amount }}</div>
         </b-col>
       </b-row>
     </b-card>
