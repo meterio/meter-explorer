92b25118da5c758a36655dc21223fc40a6d18fab
simonzg <zhanghan.simon@gmail.com>
2019-07-23 10:28:50 +0800

    update server address

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import { meterify } from "meterify";
 const Web3 = require("web3");
 
-const web3 = meterify(new Web3(), "http://n01.uubright.com:8669");
+const web3 = meterify(new Web3(), "http://test.meter.io:8669");
 
 const getBlockLatest = () => {
   return web3.eth.getBlock("latest");
