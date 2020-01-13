427fd9040a0403cf38d63fe3d73631c94a4e415b
simonzg <zhanghan.simon@gmail.com>
2020-01-13 23:57:37 +0800

    update nginx config to work with vue app

--- release.sh
+++ release.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 TEAM_NAME=dfinlab
 PROJECT_NAME=meter-scan
-VERSION=1.0.0
+VERSION=1.0.1
 
 DOCKER_TAG=${TEAM_NAME}/${PROJECT_NAME}:$VERSION
 LATEST_TAG=${TEAM_NAME}/${PROJECT_NAME}:latest
 
 npm run build
 docker build -t $LATEST_TAG .
-docker tag $DOCKER_TAG $LATEST_TAG
+docker tag $LATEST_TAG $DOCKER_TAG
 docker push $LATEST_TAG
 docker push $DOCKER_TAG
