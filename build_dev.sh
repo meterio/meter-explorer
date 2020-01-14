525185c3407f4c0ce64ca05d72145ee228b82bd9
simonzg <zhanghan.simon@gmail.com>
2020-01-14 12:53:42 +0800

    update build scripts

--- build_dev.sh
+++ build_dev.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 npm run build -- --mode development
 npm run docker:build
-npm run docker:tag
+npm run docker:tag-dev
 npm run docker:publish
-npm run postdocker:publish
+npm run docker:publish-dev
--- build_release.sh
+++ build_release.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 npm run build
 npm run docker:build
-npm run docker:tag
+npm run docker:tag-latest
 npm run docker:publish
-npm run postdocker:publish
+npm run docker:publish-latest
